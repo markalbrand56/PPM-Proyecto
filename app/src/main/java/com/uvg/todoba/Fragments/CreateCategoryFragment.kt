7d3f00429cd379f50ee05dce5846b2631f250db6
Diego Morales <mor21146@uvg.edu.gt>
2022-10-20 19:36:49 -0600

    Fragments Creados

---
+++ app/src/main/java/com/uvg/todoba/Fragments/CreateCategoryFragment.kt
@@ -0,0 +1,13 @@
+package com.uvg.todoba.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.uvg.todoba.R
+
+
+class CreateCategoryFragment : Fragment(R.layout.fragment_create_category) {
+
+}

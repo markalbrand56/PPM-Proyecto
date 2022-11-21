7a4f943debd31202f1db77e1b77f66357409713f
Mark Albrand <mark.albrand5@gmail.com>
2022-11-21 11:24:08 -0600

    inincio sesión

--- app/src/main/java/com/uvg/todoba/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/uvg/todoba/ui/fragments/LoginFragment.kt
@@ -15,15 +15,18 @@ import com.uvg.todoba.data.remote.firebase.FirebaseAuthApiImpl
 import com.uvg.todoba.data.repository.auth.AuthRepository
 import com.uvg.todoba.data.repository.auth.AuthRepositoryImpl
 import com.uvg.todoba.databinding.FragmentLoginBinding
+import com.uvg.todoba.ui.viewmodels.SessionViewModel
+import com.uvg.todoba.ui.viewmodels.states.SessionState
 import com.uvg.todoba.util.dataStore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
     private lateinit var binding: FragmentLoginBinding
-    private lateinit var authRepository: AuthRepository
+    private lateinit var sessionViewModel: SessionViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,10 +39,36 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        authRepository = AuthRepositoryImpl(
+        val authRepository = AuthRepositoryImpl(
             authAPI = FirebaseAuthApiImpl()
         )
+        sessionViewModel = SessionViewModel(authRepository, requireContext())
         setListeners()
+        setObservables()
+    }
+
+    private fun setObservables() {
+        lifecycleScope.launchWhenStarted {
+            sessionViewModel.sessionState.collectLatest { state ->
+                handleState(state)
+            }
+        }
+    }
+
+    private fun handleState(state: SessionState) {
+        when(state){
+            is SessionState.LoggedIn ->{
+                findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToHomeFragment())
+            }
+            is SessionState.Loading -> {
+                Toast.makeText(
+                    requireContext(),
+                    "Loading...",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            else -> {}
+        }
     }
 
     private fun setListeners() {
@@ -47,15 +76,7 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             val email = binding.inputLayoutLoginfragmentEmail.editText!!.text.toString()
             val password = binding.inputLayoutLoginfragmentPassword.editText!!.text.toString()
 
-            lifecycleScope.launch {
-                val response = authRepository.signInWithEmailAndPassword(email, password)
-                if (response != null) {
-                        saveKeyValue("user", response.toString())
-                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
-                }else{
-                    Toast.makeText(requireContext(), "Error al iniciar sesión", Toast.LENGTH_SHORT).show()
-                }
-            }
+            sessionViewModel.signIn(email, password)
         }
     }
 
